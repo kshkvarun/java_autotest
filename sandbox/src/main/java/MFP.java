dd2589257f1a956a06bc58be0f3969b5f5679dd0
kshkvarun <k0styan19894@gmail.com>
2019-08-21 17:40:49 +0300

    НАстройки форматирования

--- sandbox/src/main/java/MFP.java
+++ sandbox/src/main/java/MFP.java
@@ -1,7 +1,7 @@
 public class MFP {
 
-	public static void main(String[] args) {
-System.out.println("Hello, world!");
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
 
-	}
+  }
 }
