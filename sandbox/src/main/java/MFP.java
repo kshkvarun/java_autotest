34f259523be647b1853469bd3ec5560fe31a10a4
kshkvarun <k0styan19894@gmail.com>
2019-08-23 16:03:24 +0300

    Function ADDED!

--- sandbox/src/main/java/MFP.java
+++ sandbox/src/main/java/MFP.java
@@ -1,7 +1,29 @@
 public class MFP {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("World!");
+    hello("usr");
+    hello("Alex");
+
+    double l = 5;
+    System.out.println("Площадь квадрата " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь треугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+  }
+
+  public static void hello(String SB) {
+    System.out.println("Hello " + SB + " " + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double b, double a) {
+    return a * b;
 
   }
 }
